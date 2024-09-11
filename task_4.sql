feb4d9a75a162d4a9ba1671b889c5bd8d50f1563
peter kiwanda <kiwandapeter32@gmail.com>
2024-09-11 22:03:08 +0300

    Full description

--- task_4.sql
+++ task_4.sql
@@ -1,14 +1,14 @@
 -- Select detailed information about columns in the Books table
 SELECT 
     COLUMN_NAME AS 'Column Name',
-    DATA_TYPE AS 'Data Type',
+    COLUMN_TYPE AS 'Column Type',
     IS_NULLABLE AS 'Is Nullable',
     COLUMN_DEFAULT AS 'Default Value',
     CHARACTER_MAXIMUM_LENGTH AS 'Character Max Length',
     NUMERIC_PRECISION AS 'Numeric Precision',
     NUMERIC_SCALE AS 'Numeric Scale'
 FROM 
-    information_schema.COLUMNS
+    INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_SCHEMA = DATABASE() AND
+    TABLE_SCHEMA = 'alx_book_store' AND
     TABLE_NAME = 'Books';
